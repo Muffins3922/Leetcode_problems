01c196b33dbe0d7007d8556fa48e061db92d01f7
unknown <abdiel23200y@gmail.com>
2026-09-17 20:50:54 -0600

    Palindrome base

---
+++ Palindrome_number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // HERE IS THE LOGIC OF MY SOLUTION
+        
+    }
+};
+
+int main() {
+    Solution test;
+    
+    cout << boolalpha << test.isPalindrome(121) << endl;   // true
+    cout << boolalpha << test.isPalindrome(-121) << endl;  // false
+    cout << boolalpha << test.isPalindrome(10) << endl;    // false
+    
+    return 0;
+}
